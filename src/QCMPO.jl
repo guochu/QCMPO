7fa80cd90671d0cbf6ee3219f43598301db079ee
guo chu <guochu604b@gmail.com>
2023-12-25 14:38:37 +0800

    gc changes

--- src/QCMPO.jl
+++ src/QCMPO.jl
@@ -1,33 +1,19 @@
 module QCMPO
 
-
-# supported symmetries
-export FermionicSymmetry, SpinfulFermionicSymmetry, ChargeCharge, SpinCharge, SpinlessFermionicSymmetry, Charge
-export FermionicSymmetrySector, ChargeChargeSector, SpinChargeSector, space, ChargeSector
-
-
 # backends
-export hamiltonian, twobody, fourbody, creation
+export hamiltonian
 
 # interface for quantum chemistry hamiltonian
 export qcmpo
 
 using Reexport
-using SphericalTensors
-const TK = SphericalTensors
 @reexport using DMRG, Hamiltonians
 
-# definition of fermionic symmetries
-include("symmetries.jl")
-
-# backends for genenrating twobody and fourbody terms
-include("spinful.jl")
-include("spinless.jl")
 
 # interface for quantum chemistry
-include("qcinterface.jl")
-include("qcinterface/util.jl")
-include("qcinterface/chargecharge/blocks.jl")
-include("qcinterface/chargecharge/operators.jl")
+include("hamiltonian.jl")
+include("chargecharge/util.jl")
+include("chargecharge/blocks.jl")
+include("chargecharge/operators.jl")
 
 end
